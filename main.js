60b01c129c7d7ec447b02f716935c37626eedf17
rccacho <rccacho@gmail.com>
2017-02-01 19:00:29 -0800

    added images and formatted

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+  	alert("Move Cookie Monster by pressing on 'Enter'. Move Elmo by pressing on 'Space'.")
 
    $("body").on("keypress", function handleKeypress(event) {
       if(event.which == 13) {
         $( "#player1" ).animate({ "left": "+=50px" }, "fast" );	  
         var position1 = $("#player1").offset();		
-		if (position1.left >= 1000) {
-   		  alert("Win!");
+		if (position1.left >= 875) {
+   		  alert("Cookie Monster Wins!");
 	  }
    	}
     });
@@ -18,15 +18,11 @@ $(document).ready(function() {
       if (event.which == 0 || event.which == 32) {
         $( "#player2" ).animate({ "left": "+=50px" }, "fast" );
         var position2 = $("#player2").offset();	
-		if (position2.left >= 1000) {
-   		  alert("Win!");
+		if (position2.left >= 875) {
+   		  alert("Elmo Wins!");
   	  }
   	}
     });
-
-
-
-
 
 
 });
